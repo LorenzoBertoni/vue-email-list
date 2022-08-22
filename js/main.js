8c86b625b64ba0a6f8998ec070dab521ca2ea1da
LorenzoBertoni <lorenzo.berto99@hotmail.it>
2022-08-22 14:42:52 +0200

    add: ciclo for per la chiamata all'API

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 const app = new Vue({
     el: '#app',
-    
+    data: {
+        emails: [],
+    },
+    created() {
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => {
+                this.emails.push(response.data.response);
+            })
+        }
+    }
 })
