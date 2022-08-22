c6b74d660e79749d4543bcf08bcf279279f78ec3
LorenzoBertoni <lorenzo.berto99@hotmail.it>
2022-08-22 15:20:15 +0200

    rimossi i commenti

--- js/main.js
+++ js/main.js
@@ -2,14 +2,12 @@ const app = new Vue({
     el: '#app',
     data: {
         emails: [],
-        //isReady: false
     },
     created() {
         for (let i = 0; i < 10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then(response => {
                 this.emails.push(response.data.response);
-                //this.isReady = true
             })
         }
     }
